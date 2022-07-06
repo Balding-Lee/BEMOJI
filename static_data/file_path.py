acc14a28fcc1da8ea5bfb2df52dfbf3bd00b0226
George Lee <49380927+Balding-Lee@users.noreply.github.com>
2022-07-06 16:50:39 +0800

    Add files via upload

---
+++ static_data/file_path.py
@@ -0,0 +1,23 @@
+"""
+链接数据
+:author: Qizhi Li
+"""
+crawl_dir = '../crawl_data/'
+weibo_emoji2def = '../crawl_data/emoji2def.csv'
+github_emoji2def = '../crawl_data/github_emoji2def.csv'
+weibo_data = '../static_data/weibo_data/'
+english_data = '../static_data/english_dataset/'
+github_data = '../static_data/english_dataset/github/'
+pre_train_dir = '../pre-train_data/'
+pre_train_chinese_data = '../pre-train_data/pre_train_data.txt'
+pre_train_english_data = '../pre-train_data/githubdata_emoji.txt'
+pre_train_parameters = '../models/pretrain_parameters/'
+model_parameters = '../models/model_parameters/'
+deepmoji_vocab = '../static_data/deepmoji_vocab/'
+chinese_word2vec = '../static_data/word2vec/w2v.pkl'
+english_word2vec = '../static_data/word2vec/GoogleNews-vectors-negative300.bin'
+
+glue_data = '../static_data/glue_data/'
+chinese_sa_dataset = '../static_data/Chinese_SA_dataset/'
+
+case_study = '../static_data/case_study/'
